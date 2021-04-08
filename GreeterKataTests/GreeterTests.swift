31a553666a8cc252eb75ac572da244bc00b8fc2a
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-04-08 14:48:11 -0500

    Refactor tests and add date helper

--- GreeterKataTests/GreeterTests.swift
+++ GreeterKataTests/GreeterTests.swift
@@ -15,11 +15,16 @@ final class GreeterTests: XCTestCase {
 
     func test_greet_with1159am_shouldSayGoodMorning() {
         let sut = Greeter(name: "")
-        let components = DateComponents(calendar: .current, hour: 11, minute: 59)
-        let time = components.date!
         
-        let result: String = sut.greet(time: time)
+        let result = sut.greet(time: date(hour: 11, minute: 59))
         
         XCTAssertEqual(result, "Good Morning")
+    }
+    
+    // MARK:- Helpers
+    
+    private func date(hour: Int, minute: Int) -> Date {
+        let components = DateComponents(calendar: .current, hour: hour, minute: minute)
+        return components.date!
     }
 }
