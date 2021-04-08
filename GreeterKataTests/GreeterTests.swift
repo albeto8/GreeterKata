a8aa1d7124b3c624d17ae4b89453e8bb79ffecc4
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-04-08 14:53:15 -0500

    Second test passing

--- GreeterKataTests/GreeterTests.swift
+++ GreeterKataTests/GreeterTests.swift
@@ -7,6 +7,10 @@ struct Greeter {
     }
     
     func greet(time: Date) -> String {
+        let components = Calendar.current.dateComponents([.hour], from: time)
+        if components.hour == 12 {
+            return "Good Afternoon"
+        }
         return "Good Morning"
     }
 }
@@ -19,6 +23,14 @@ final class GreeterTests: XCTestCase {
         let result = sut.greet(time: date(hour: 11, minute: 59))
         
         XCTAssertEqual(result, "Good Morning")
+    }
+    
+    func test_greet_with1200pm_shouldSayGoodAfternoon() {
+        let sut = Greeter(name: "")
+        
+        let result = sut.greet(time: date(hour: 12, minute: 00))
+        
+        XCTAssertEqual(result, "Good Afternoon")
     }
     
     // MARK:- Helpers
