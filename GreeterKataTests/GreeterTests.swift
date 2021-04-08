261c0a5ca9e4fdbf7bf9739efb68f16392416192
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-04-08 14:37:36 -0500

    First failing test

--- GreeterKataTests/GreeterTests.swift
+++ GreeterKataTests/GreeterTests.swift
@@ -1,9 +1,25 @@
 @testable import GreeterKata
 import XCTest
 
+struct Greeter {
+    init(name: String) {
+        
+    }
+    
+    func greet(time: Date) -> String {
+        return ""
+    }
+}
+
 final class GreeterTests: XCTestCase {
 
-    func test_zero() throws {
-        XCTFail("Tests not yet implemented in GreeterTests")
+    func test_greet_with1159am_shouldSayGoodMorning() {
+        let sut = Greeter(name: "")
+        let components = DateComponents(calendar: .current, hour: 11, minute: 59)
+        let time = components.date!
+        
+        let result: String = sut.greet(time: time)
+        
+        XCTAssertEqual(result, "Good Morning")
     }
 }
