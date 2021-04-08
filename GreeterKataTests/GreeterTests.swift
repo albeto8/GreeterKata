1acaf9b940ea14a6f3042bff0c487d5e8cfd2bb2
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-04-08 14:30:33 -0500

    Test zero

---
+++ GreeterKataTests/GreeterTests.swift
@@ -0,0 +1,9 @@
+@testable import GreeterKata
+import XCTest
+
+final class GreeterTests: XCTestCase {
+
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in GreeterTests")
+    }
+}
