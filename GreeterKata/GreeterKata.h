02f4f41ef2be0183566f08c84b406afae57137cf
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-04-08 14:29:12 -0500

    Initial Commit

---
+++ GreeterKata/GreeterKata.h
@@ -0,0 +1,18 @@
+//
+//  GreeterKata.h
+//  GreeterKata
+//
+//  Created by Mario Alberto Barragán Espinosa on 08/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GreeterKata.
+FOUNDATION_EXPORT double GreeterKataVersionNumber;
+
+//! Project version string for GreeterKata.
+FOUNDATION_EXPORT const unsigned char GreeterKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreeterKata/PublicHeader.h>
+
+
