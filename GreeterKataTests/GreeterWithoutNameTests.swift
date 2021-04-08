efce30f660455d2f339292e42f452adc02474298
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-04-08 17:15:13 -0500

    Rename test file

--- GreeterKataTests/GreeterWithoutNameTests.swift
+++ GreeterKataTests/GreeterWithoutNameTests.swift
@@ -32,7 +32,7 @@ struct Greeter {
     }
 }
 
-final class GreeterTests: XCTestCase {
+final class GreeterWithoutNameTests: XCTestCase {
 
     func test_greet_with1159am_shouldSayGoodMorning() {
         let sut = Greeter(name: "")
